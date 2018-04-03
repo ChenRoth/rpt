95a8b5e675f15be5def45d95c8b2fe21e6eb4773
Chen Roth <chenro@wix.com>
2018-04-03 22:20:25 +0300

    boilerplate

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react'
+import { render } from 'react-dom'
+
+const App = () => {
+  return <div>This is my app!</div>
+}
+const root = document.getElementById('root')
+render(<App/>, root)
